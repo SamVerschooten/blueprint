a428835fabb27f4c163d41ab01623bd6c74ecbb5
Sam Verschooten <sam@hatchsoftware.eu>
2016-09-16 15:18:13 +0200

    angular2 hello world with systemjs

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname));
 
 app.listen(port, function () {
     console.log('Blueprint app listening on port ' + port + '!');
