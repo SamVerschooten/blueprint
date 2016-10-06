331fbc728f3237ba6ed1f8e0ed6ca8442dfe763b
sversch2 <sam.verschooten@telenetgroup.be>
2016-10-06 15:55:16 +0200

    Added sidenav toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,27 @@
-import {Component, ViewEncapsulation} from '@angular/core';
+import {Component, ViewChild, ViewEncapsulation} from '@angular/core';
+//import {MdSidenav} from '@angular/material/sidenav';
 
 @Component({
     styles: [require("./app.scss")],
     selector: 'my-app',
     encapsulation: ViewEncapsulation.None,
-    template: '<h1>Hallowkes!!</h1>'
+    template: `
+		<md-sidenav-layout>
+		    <md-sidenav #start (open)="mybutton.focus()">
+		      Start Sidenav.
+		      <button md-button #mybutton (click)="start.close()">Close</button>
+		    </md-sidenav>		   
+		    
+		   	<button md-button (click)="start.toggle()">Toggle Start Side Drawer</button>
+	  	</md-sidenav-layout>
+    `
 })
-export class AppComponent { }
+export class AppComponent { 
+
+	//@ViewChild('start')
+	//sideNav:MdSidenav
+
+	// toggle() {
+	// 	this.sideNav.toggle();
+	// }
+}
